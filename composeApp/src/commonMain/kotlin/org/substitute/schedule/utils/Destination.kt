9ee3e35e1365a1a222c60334e6d29141bef67999
KyNarec <jim48@gmx.de>
2025-10-10 18:15:19 +0200

    Define sealed interface for navigation destinations
    This commit introduces a `Destination` sealed interface to model the app's navigation graph.
    
    It defines three type-safe, serializable destinations:
    - `Today(url: String)`
    - `Tomorrow(url: String)`
    - `Settings(noCredentials: Boolean)`

---
+++ composeApp/src/commonMain/kotlin/org/substitute/schedule/utils/Destination.kt
@@ -0,0 +1,14 @@
+package org.substitute.schedule.utils
+
+import kotlinx.serialization.Serializable
+
+sealed interface Destination {
+    @Serializable
+    data class Today(val url: String = "") : Destination
+
+    @Serializable
+    data class Tomorrow(val url: String = "") : Destination
+
+    @Serializable
+    data class Settings(val noCredentials: Boolean) : Destination
+}
